53962c0db8e45e282c877bd75a40ef6a68418d2b
rams9 <ram.miriyala117@gmail.com>
2018-02-27 08:54:40 +0530

    Add files via upload

---
+++ Calc.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Calc.aspx.cs" Inherits="Calc" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:TextBox ID="txt1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txt2" runat="server"></asp:TextBox>
+        <h3 id="Result" runat="server"></h3>
+        <asp:Button ID="btn1" runat="server" Text="Add" OnClick="btn1_Click" />
+        <asp:Button ID="btn2" runat="server" Text="Sub" OnClick="btn2_Click" />
+        <asp:Button ID="btn3" runat="server" Text="Mul" OnClick="btn3_Click" />
+        <h4 runat="server" id="defaultUser"></h4>
+        <h2 runat="server" id="current"></h2>
+
+        <h1>Data Types</h1>
+         <ul>
+             <li runat="server" id="Appint">int</li>
+             <li>float</li>
+             <li>Double</li>
+             <li>char</li>
+         </ul>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Calc.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BasicData; //my own namespace 3 methods are there
+
+
+// BasicData is a Name Space
+public partial class Calc : System.Web.UI.Page
+{
+
+    Class1 cs = new Class1();
+    //Class1 is a class in BasicData Namespace
+    //class cotains fields,methods,evnts
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Call the constructer that has no parameters
+        Person ps = new Person();
+        defaultUser.InnerText = ps.Name;
+       
+        ps.SetName("Ram");
+        current.InnerText =ps.Name;
+
+        //Call The constructer that has one parameter
+        Person ps2 = new Person("Miriyala's");
+        DataTypes();
+
+    }
+
+
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(txt1.Text);
+        int b = Convert.ToInt32(txt2.Text);
+        var c = cal(a, b);
+        Result.InnerText="Addition is " +c.ToString();
+    }
+    
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(txt1.Text);
+        int b = Convert.ToInt32(txt2.Text);
+        var c = cs.Sub(a, b);
+        Result.InnerText = "Subtraction is " + c.ToString();
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(txt1.Text);
+        int b = Convert.ToInt32(txt2.Text);
+        var c = cs.Multiplication(a, b);
+        Result.InnerText = "Multiplication is " + c.ToString();
+    }
+
+    //method 
+    public int cal(int a, int b)
+    {
+
+        return cs.Add(a, b);
+
+    }
+
+    public void DataTypes ()
+
+    {
+        Appint.InnerText ="Int16 size is: "+  sizeof(Int16).ToString() + " bytes Int32 size is: " + sizeof(Int32).ToString() + " Int64 size is: " + sizeof(Int64).ToString();
+
+       
+
+    }
+
+}
